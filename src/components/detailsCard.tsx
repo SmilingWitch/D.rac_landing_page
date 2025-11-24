418d7d1339e501efb5722d55585dbbf0da004ea0
SmilingWitch <danielitayero@gmail.com>
2025-11-24 18:04:13 -0300

    feat: make responsive the comp detailsCard.tsx

--- src/components/detailsCard.tsx
+++ src/components/detailsCard.tsx
@@ -1,28 +1,33 @@
 import Image from "next/image";
-import image from "../../public/Hifi.svg"
 import DetailsCardProps from "./types/types";
 
 
 export default function DetailsCard({ src,header, text }: DetailsCardProps){
     return(
-        <div className=" relative h-[17vh] flex flex-col justify-between">
+        <div className="relative h-[17vh] flex flex-col justify-between 
+                        h-[250]
+                        2xl:h-[200]
+                        ">
             <div>
                 <h3 className="text-[white] text-[2rem]">{header}</h3>
                 <span className="text-[white] top-[5]">{text}</span>
             </div>
             <div className="mt-5">
                 <div className="bg-[#FFFF00] w-full h-[40] rounded-[20]">
-
-                </div>
                     <Image 
                         src = {src} 
                         width={150} 
                         height={150} 
                         alt = "image"
-                        className=" absolute bottom-[-80] right-[30]"
+                        className=" absolute bottom-[-80] right-[30]
+                                    sm:bottm-[-80]
+                                    lg:bottom-[-80]"
                         >
                             
                         </Image>
+                </div>
+                
+                    
             </div>
             
         </div>
