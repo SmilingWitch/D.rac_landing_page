e1fc5f59c761a679703e4d09044402b85143399c
SmilingWitch <danielitayero@gmail.com>
2025-12-19 19:04:38 -0300

    feat: add Home option to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { IoMenu } from "react-icons/io5";
 import { RiTranslate } from "react-icons/ri";
 
@@ -14,12 +15,15 @@ export default function Navbar(){
                     <RiTranslate className="text-[1.2rem]"/>
                     <span className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>EN</span> 
                 </div>
-                <div className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }} >
+                <Link href="/" className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }} >
+                    Home
+                </Link>
+                <Link href="/blog" className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }} >
                     Blog
-                </div>
-                <div className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>
+                </Link>
+                <Link href="/forum" className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>
                     Forum
-                </div>
+                </Link>
                 <button className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>Subscribe</button>
                
             </div>
