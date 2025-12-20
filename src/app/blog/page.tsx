ffba2888c1a34bb46a958f7009be8f4e4e4d3bc7
SmilingWitch <danielitayero@gmail.com>
2025-12-20 15:00:41 -0300

    feat: add blog/

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,17 @@
+import Explore from "@/components/blog/explore";
+import Principal from "@/components/blog/principal";
+import Quickies from "@/components/blog/quickies";
+import Trending from "@/components/blog/trending";
+
+
+export default function Blog() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-start bg-zinc-50 font-sans dark:bg-black">
+      <Principal/>
+      <Trending/>
+      <Quickies/>
+      <Explore/>
+      
+    </div>
+  );
+}
