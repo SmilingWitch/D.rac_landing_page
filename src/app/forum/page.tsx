6a7cf8df4305bbf806f7a15571e33ce78e0513e2
SmilingWitch <danielitayero@gmail.com>
2025-12-20 15:01:02 -0300

    feat: add forum/

---
+++ src/app/forum/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Forum() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Forum</h1>
+      
+    </div>
+  );
+}
