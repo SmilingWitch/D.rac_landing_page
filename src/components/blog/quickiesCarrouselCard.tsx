6faac45a875bfbd483710b40adb34954ada19261
SmilingWitch <danielitayero@gmail.com>
2025-12-22 12:34:52 -0300

    feat: make quickiesCarrouselCard.tsx responsive

--- src/components/blog/quickiesCarrouselCard.tsx
+++ src/components/blog/quickiesCarrouselCard.tsx
@@ -3,7 +3,9 @@ import image from "../../../public/Blog1.svg"
 
 export default function QuickiesCarrouselCard(){
     return(
-        <div className="relative w-[200px] h-[70vh] cursor-pointer overflow-hidden bg-red-300">
+        <div className="relative w-[30vw] h-[70vh] cursor-pointer overflow-hidden
+                        sm:w-[30vw]
+                        lg:w-[10vw]">
             <Image
                 src={image}
                 alt="image"
