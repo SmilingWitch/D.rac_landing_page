cc59cc5961f953f085ecc6d0d18ae9d3e0de5270
SmilingWitch <danielitayero@gmail.com>
2025-12-22 12:36:46 -0300

    refactor: blogCard component

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -5,7 +5,7 @@ import { GoArrowRight } from "react-icons/go";
 export default function BlogCard(){
 
     return(
-        <div className=" flex flex-row w-[90vw] h-[50vh] rounded-[20] border-2 border-white relative overflow-hidden items-end
+        <div className="flex flex-row w-[90vw] h-[50vh] rounded-[20] border-2 border-white relative overflow-hidden items-end
                         sm:w-[70vw] sm:h-[50vh]
                         lg:w-[40vw] lg:h-[40vh]
                         xl:w-[40vw] xl:h-[40vh]
@@ -23,7 +23,7 @@ export default function BlogCard(){
             </div>
             
             <div className="flex flex-col justify-between items-end h-full p-[5] w-full ">
-                <div className="flex flex-col gap-[5]  justify-center w-full relative">
+                <div className="flex flex-col gap-[5] justify-center w-full relative">
                     <span className="text-[1.5rem] ">Alicia Alonso resurrects</span>
                     <span className="w-full p-[7]">On our blog, you'll find relevant information on all kinds of events: from festivals and conferences to product launches and brand experiences. We analyze the market, explore notable occurrences, and tell you what's on everyone's rada</span>
                 </div>
