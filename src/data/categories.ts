b3e62c1746ecd3adf3d54322b1828ad9a4e34ecb
SmilingWitch <danielitayero@gmail.com>
2025-12-22 12:42:05 -0300

    feat: add categories.ts

---
+++ src/data/categories.ts
@@ -0,0 +1,28 @@
+export const categories = [
+  
+  { 
+    id: 1,
+    imageUrl: "/CommunityAnnouncements.svg",
+    name: "Community Announcements",
+    text: "Important messages to community users",  
+    number: 76 },
+  { 
+    id: 2,
+    imageUrl: "/Questions&Answers.svg",   
+    name: "Questions & Answers",       
+    text: "Find answers and get expert advice",
+    number: 204, },
+    
+  {
+    id: 3, 
+    imageUrl: "/EventsNews.svg",   
+    name: "Events News",      
+    text: "Stay informed about the latest experiences" ,
+    number: 200},
+{ 
+    id: 4,
+    imageUrl: "/DiverseTopics.svg",   
+    name: "Diverse Topics",      
+    text: "Stay informed about the latest experiences" ,
+    number: 137 }
+];
