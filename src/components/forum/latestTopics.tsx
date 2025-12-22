b0ca6761daede836e80df0a170b31fb8d2f73131
SmilingWitch <danielitayero@gmail.com>
2025-12-22 12:44:25 -0300

    feat: add latestTopics.tsx

---
+++ src/components/forum/latestTopics.tsx
@@ -0,0 +1,36 @@
+import { FaArrowRight } from "react-icons/fa";
+import CategoryCard from "./categoryCard";
+import { categories } from "@/data/categories";
+import CardForum from "./forumCard";
+
+
+export default function LatestTopic(){
+    return(
+        <div className="flex flex-col mt-[50px] w-full min-h-50vh min-h-[200vh]">
+            <div className="flex items-center justify-center gap-[10] w-[300px] mb-[50px]">
+                <h2 className="relative font-bold 
+                                text-[2rem]
+                                sm:text-[2rem] 
+                                md:text-[2rem] 
+                                lg:text-[2.5rem]">
+                    Latest Topics
+                </h2>
+                <div className="text-[1rem]
+                                sm:text-[1rem] 
+                                md:text-[1rem] 
+                                lg:text-[1.5rem]
+                                text-[#ffff00]
+                                mt-[10]">
+                    <FaArrowRight/>
+                </div>
+            </div>
+            <div className="relative flex flex-col gap-[20px] justify-center items-center w-full px-[50px]
+                                        sm:flex-col
+                                        lg:flex-row">
+                <CardForum/>
+                <CardForum/>
+                <CardForum/>
+            </div>
+        </div>
+    )
+}
