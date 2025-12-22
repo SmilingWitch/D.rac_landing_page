e496d109d6cdea9730b4f22ffb9c1bbf585e78c7
SmilingWitch <danielitayero@gmail.com>
2025-12-22 12:28:32 -0300

    feat: add components to forum page

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,10 +1,16 @@
+import Categories from "@/components/forum/categories";
+import LatestTopic from "@/components/forum/latestTopics";
+import PrincipalForum from "@/components/forum/principal";
+import TopVoted from "@/components/forum/topVoted";
 
 
 export default function Forum() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Forum</h1>
-      
+    <div className="flex flex-col min-h-screen items-start justify-start bg-zinc-50 font-sans dark:bg-black">
+      <PrincipalForum/>
+      <TopVoted/>
+      <Categories/>
+      <LatestTopic/>
     </div>
   );
 }
