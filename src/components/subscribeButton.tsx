ed691dfa22555cb8b0cdb5dd60e8fbf09253bf30
SmilingWitch <danielitayero@gmail.com>
2025-12-22 12:41:44 -0300

    feat: add subscribeButton.tsx

---
+++ src/components/subscribeButton.tsx
@@ -0,0 +1,16 @@
+
+
+export default function SubscribeButton(){
+    return(
+        
+        <button
+            className="
+              font-bold bg-[#FFFF00] text-black
+              w-[100px] rounded-[5px] p-[5px] cursor-pointer
+              hover:brightness-95 active:scale-[0.98] transition
+            "
+          >
+            Subscribe
+        </button>
+    )
+}
