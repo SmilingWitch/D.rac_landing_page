49e821c842525ff69702c9f6598421f90e412411
SmilingWitch <danielitayero@gmail.com>
2025-12-26 16:04:25 -0300

    feat: add desktop navbar ang mobile navbar to navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
-
-
+"use client"
+import { usePathname } from "next/navigation"
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -11,47 +11,19 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import Link from "next/link";
-import { IoMenu } from "react-icons/io5";
 import { RiTranslate } from "react-icons/ri";
 import SubscribeButton from "./subscribeButton";
+import ComputerNavbar from "./computerNavbar";
+import MobileNavbar from "./mobileNavBar";
 
-
-/*<div className="flex gap-[5] justify-center items-center cursor-pointer">
-                    <RiTranslate className="text-[1.2rem]"/>
-                    <span className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>EN</span> 
-                </div>*/
 
 export default function Navbar(){
+    const pathname = usePathname()
+
     return(
         <div className="w-full flex justify-end">
-            <NavigationMenu className="px-6 py-3 w-[100%] relative flex justify-end">
-                <NavigationMenuList className=" w-full relative">
-                    <NavigationMenuItem>
-                        <div className="flex gap-[5] justify-center items-center cursor-pointer">
-                            <RiTranslate className="text-[1.2rem]"/>
-                            <span className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>EN</span> 
-                        </div>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink>
-                            <Link  href="/" className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>Home</Link>
-                        </NavigationMenuLink> 
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink>
-                            <Link href="/blog" className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>Blog</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink>
-                            <Link href="/forum" className="cursor-pointer font-bold text-[1.2rem]" style={{ fontFamily: 'var(--font-barlow)' }}>Forum</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <SubscribeButton/>
-                    </NavigationMenuItem>                    
-                </NavigationMenuList>
-            </NavigationMenu>
+            <ComputerNavbar/>
+            <MobileNavbar/>
         </div>
     )
 }
