b9b18376c76d54de1aec40f0b65d875be10dd973
SmilingWitch <danielitayero@gmail.com>
2025-12-26 15:59:22 -0300

    fix: fix the style of app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import PrincipalView from "@/components/principalView";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="relative flex flex-col min-h-screen items-start justify-start bg-zinc-50 font-sans dark:bg-black">
       <PrincipalView/>
       <BuyingSellingDetails/>
       <PilotUser/>
