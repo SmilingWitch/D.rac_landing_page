9659cb716f8c9cda6154b687801a2ae953ae8479
SmilingWitch <danielitayero@gmail.com>
2025-12-26 18:02:13 -0300

    feat: make forumCard responsive

--- src/components/forumCard.tsx
+++ src/components/forumCard.tsx
@@ -4,26 +4,35 @@ import { IoStar } from "react-icons/io5";
 
 export default function ForumCard(){
     return(
-        <div className="relative w-[100vw] flex flex-col justify-center items-center h-[30vh] gap-[40]">
-            <span className="text-[1.5rem] w-[50vw] block text-center italic">
+        <div className="relative w-[100vw] flex flex-col justify-center items-center min-h-[30vh] gap-[40]">
+            <span className="text-[1] w-[50vw] block text-center italic
+                                sm:text-[1rem]
+                                lg:text-[1.2rem]
+                                xl:text-[1.5rem]  ">
                 I signed up for the beta and have been receiving exciting updates ever since. 
                 What excites me most is the variety of events they offer—much more than other apps! 
                 If they deliver on everything they promise, it's going to be a hit.
             </span>
-            <div className="flex gap-[30px] items-center  ">
-                <div>
+            <div className="flex gap-[30px] items-center">
+                <div className="relative w-[70px] h-[70px] rounded-[50%] overflow-hidden
+                                sm:w-[70px] sm:h-[70px]
+                                xl:w-[100px] xl:h-[100px] ">
                     <Image
                         src = {image}
                         alt = "image"
-                        width={100}
-                        height={100}
-                        className="rounded-[50%] overflow-hidden"
+                        fill
+                        className="object-cover"
                     ></Image>
 
                 </div>
                 <div>
-                    <span className="text-[1.2rem]">Oprah Winfrey</span>
-                    <div className="flex text-[2rem] text-[#FFFF00] gap-[2] text-[#191919]">
+                    <span className="text-[1rem]
+                                    sm:text-[1rem]
+                                    lg:text-[1.2rem]">Oprah Winfrey</span>
+                    <div className="flex text-[1.5rem] text-[#FFFF00] gap-[2] text-[#191919]
+                                    sm:text-[1.5rem]
+                                    lg:text-[1.8rem]
+                                    xl:text-[2rem]">
                         <IoStar />
                         <IoStar />
                         <IoStar />
