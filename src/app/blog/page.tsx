1f4f95913fabba739aa2bab8afb46472a05e07b0
SmilingWitch <danielitayero@gmail.com>
2025-12-26 15:58:33 -0300

    fix: fix the style of blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ import Trending from "@/components/blog/trending";
 
 export default function Blog() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-start bg-zinc-50 font-sans dark:bg-black">
+    <div className="relative flex flex-col min-h-screen items-center justify-start bg-zinc-50 font-sans dark:bg-black">
       <Principal/>
       <Trending/>
       <Quickies/>
